d48cd85cd5be54aea66ca4e03d7be4b9caacdbbf
MuhammadAlfarazi-rpl <mdrsff@gmail.com>
2025-06-27 13:35:14 +0700

    Great Reset

--- includes/header.php
+++ includes/header.php
@@ -5,11 +5,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-  
+ 
   <!-- Google Fonts -->
-<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Outfit:wght@500;700&display=swap" rel="stylesheet">
-
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Outfit:wght@500;700&display=swap" rel="stylesheet">
 
   <!-- Title -->
   <title>Porto</title>
--- index.php
+++ index.php
@@ -1,23 +1,6 @@
 
 <?php include 'includes/header.php'; ?>
-<div class="container-xxl mt-5 fade-up-init">
-  <div class="d-flex flex-wrap align-items-center justify-content-between custom-hero p-2">
-    <!-- Teks -->
-    <div class="hero-card-wrapper text-block">
-      <div class="hero-card card shadow" data-tilt data-tilt-max="15" data-tilt-scale="1.01">
-      <h1 class="ml6 expandable-title title-big">
-        <span class="letters title-1">Halo, Saya Aris!</span>
-      </h1>
-      <h3 class="ml6 expandable-title title-small">
-        <span class="letters title-2">"Almost"-Fullstack Dev</span>
-      </h3>
-      <hr></hr>
-      <div class="lead description-text-blowup" style="opacity: 0; visibility: hidden;">
-      <p>"Front-End, Back-End, Database. All in one man army."</p>
-      <p>Halo! Saya aris, siswa smkn4 tanjung pinang</p>
-      </div>
-    </div>
-  </div>
-  </div>
+<div class="container-xxl mt-5">
+  
 </div>
 <?php include 'includes/footer.php'; ?>
